43d57f9d813cff6ec753c4c27f6797eb102c958c
Robert Beekman <robert@matsimitsu.nl>
2015-09-10 15:51:41 +0200

    Add logging

--- lib/appsignal/integrations/rake.rb
+++ lib/appsignal/integrations/rake.rb
@@ -14,10 +14,15 @@ module Rake
         :action => name,
         :params => args
       )
-
+      Appsignal.logger.info("Invoking rake task with AppSignal")
       invoke_without_appsignal(*args)
     rescue => exception
+      Appsignal.logger.info("Excepion caught")
+      Appsignal.logger.info("Appsignal active? #{Appsignal.active?.inspect}")
+      Appsignal.logger.info("Ignored exception? #{Appsignal.is_ignored_exception?(exception).inspect}")
+
       if Appsignal.active? && !Appsignal.is_ignored_exception?(exception)
+        Appsignal.logger.info("Added exception to transaction")
         transaction.add_exception(exception)
       end
       raise exception
