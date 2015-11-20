1b6c23624ab6b68c72cae06dec9885265c959250
Thijs Cadier <thijs@appsignal.com>
2015-11-20 17:09:56 +0100

    Move to hooks for integration with dependencies
    To avoid potential load problems move to hooks system that can be
    called at the appropriate moment.

---
+++ lib/appsignal/hooks.rb
@@ -0,0 +1,55 @@
+module Appsignal
+  class Hooks
+    class << self
+      def register(name, hook)
+        @hooks ||= {}
+        @hooks[name] = hook
+      end
+
+      def load_hooks
+        @hooks.each do |name, hook|
+          hook.try_to_install(name)
+        end
+      end
+
+      def hooks
+        @hooks ||= {}
+      end
+    end
+
+    class Hook
+      def self.register(name, hook=self)
+        Appsignal::Hooks.register(name, hook.new)
+      end
+
+      def try_to_install(name)
+        if dependencies_present? && !installed?
+          Appsignal.logger.info("Installing #{name} integration")
+          install
+          @installed = true
+        end
+      end
+
+      def installed?
+        !! @installed
+      end
+
+      def dependencies_present?
+        raise NotImplementedError
+      end
+
+      def install
+        raise NotImplementedError
+      end
+    end
+  end
+end
+
+require 'appsignal/hooks/celluloid'
+require 'appsignal/hooks/delayed_job'
+require 'appsignal/hooks/passenger'
+require 'appsignal/hooks/puma'
+require 'appsignal/hooks/rake'
+require 'appsignal/hooks/resque'
+require 'appsignal/hooks/sidekiq'
+require 'appsignal/hooks/unicorn'
